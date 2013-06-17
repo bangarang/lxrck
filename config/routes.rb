9b8b895b430a6df4b54531bdd496d201bfa5bbcb
bangarang <me@alexjrock.com>
2013-06-16 22:04:29 -0500

    iOS no video

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,3 @@
 Lxrck::Application.routes.draw do
-  	resources :messages
-
-
 	root :to => 'pages#home'
-	match 'tellme' => 'pages#tellme'
-	match 'bgs' => 'pages#backgroundvideo'
-	match 'bg2' => 'pages#bg2'
-	match 'bg3' => 'pages#bg3'
-	match 'icons' => 'pages#icons'
-	match 'example' => 'pages#example'
-
 end
