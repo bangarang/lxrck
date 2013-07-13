8bc3a903daa9826de3a8fd153463de691dda5f7e
bangarang <me@alexjrock.com>
2013-07-13 17:20:17 -0500

    little menu action

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ gem 'rails', '3.2.11'
 gem 'jquery-rails'
 gem 'unicorn'
 gem 'font-awesome-rails', "~> 3.1.1.3"
-gem 'clearance', '1.0.0.rc7'
+gem 'clearance', :path => "vendor/gems/clearance-1.0.0.rc7"
 
 group :development, :test do
   gem 'sqlite3', '1.3.5'
---
+++ vendor/gems/clearance-1.0.0.rc7/lib/clearance/configuration.rb
@@ -0,0 +1,31 @@
+module Clearance
+  class Configuration
+    attr_accessor \
+      :cookie_expiration,
+      :mailer_sender,
+      :password_strategy,
+      :redirect_url,
+      :secure_cookie,
+      :user_model
+
+    def initialize
+      @cookie_expiration = lambda { 1.year.from_now.utc }
+      @mailer_sender = 'reply@example.com'
+      @secure_cookie = false
+      @redirect_url = '/'
+    end
+
+    def user_model
+      @user_model || ::User
+    end
+  end
+
+  class << self
+    attr_accessor :configuration
+  end
+
+  def self.configure
+    self.configuration ||= Configuration.new
+    yield configuration
+  end
+end
