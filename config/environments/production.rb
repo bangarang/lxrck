1de03230646915e23357d1a3399f7b150c574e52
bangarang <me@alexjrock.com>
2013-08-19 20:37:23 -0500

    paperclip test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,12 +67,15 @@ Lxrck::Application.configure do
 
   config.paperclip_defaults = {
     :storage => :s3,
+    :s3_protocol => 'http',
     :s3_credentials => {
       :bucket => ENV['AWS_BUCKET'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
   }
+  Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+  Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
 
   config.assets.precompile += %w( modernizr.js )
 
