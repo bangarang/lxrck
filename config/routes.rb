a58438f5e9d39a27c0315d63207c5dc3eb520c7e
bangarang <me@alexjrock.com>
2013-08-20 21:01:05 -0500

    env variables

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Lxrck::Application.routes.draw do
 	# root :to => 'pages#home'
 	match 'in' => 'pages#in'
 
-  get '/sign_in' => 'clearance/sessions#new', :as => 'born'
-  get '/sign_up' => 'clearance/users#new', :as => 'sign_up'
-  delete '/sign_out' => 'clearance/sessions#destroy', :as => 'kill'
+	get '/sign_in' => 'clearance/sessions#new', :as => 'born'
+	# get '/sign_up' => 'clearance/users#new', :as => 'sign_up'
+	delete '/sign_out' => 'clearance/sessions#destroy', :as => 'kill'
 	resources :tags, :path => "/"
 
 	# root :controller => "tags", :action => "index"
