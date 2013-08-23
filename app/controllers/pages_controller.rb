ea21f86d895eac2999dc6a7c6bd849156c36fd8a
bangarang <me@alexjrock.com>
2013-08-23 09:43:09 -0500

    route changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,13 @@ class PagesController < ApplicationController
 
   before_filter :authorize, :except => :home
   def home
+  	@tags = Tag.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @tags }
+    end
+
   end
 
   def in
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Lxrck::Application.routes.draw do
   	resources :posts
   	resources :photos
-	# root :to => 'pages#home'
+	root :to => 'pages#home'
 	match 'in' => 'pages#in'
 
 	get '/sign_in' => 'clearance/sessions#new', :as => 'born'
 	# get '/sign_up' => 'clearance/users#new', :as => 'sign_up'
 	delete '/sign_out' => 'clearance/sessions#destroy', :as => 'kill'
-	resources :tags, :path => "/"
+	resources :tags
+
 
 	# root :controller => "tags", :action => "index"
 	# match "/:id" as :tags
