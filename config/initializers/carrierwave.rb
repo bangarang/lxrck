9739c7818c80ed6f6e1cbd4c7ec06dd3af810e79
bangarang <me@alexjrock.com>
2013-08-23 10:39:09 -0500

    carrierwave settings

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,7 @@ CarrierWave.configure do |config|
     aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
     :region                 => 'us-west-2',
     :host                   => 'http://assets.lxrck.com',
-    # :endpoint               => 'http://assets.lxrck.com'
+    :endpoint               => 'http://assets.lxrck.com'
   }
   config.fog_directory = ENV['AWS_BUCKET']
 end
