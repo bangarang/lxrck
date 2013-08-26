577ce901065659ca8188c7c832bc158910bd6b81
bangarang <me@alexjrock.com>
2013-08-26 00:45:03 -0500

    Mark Simonson fonts and Triangle

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
   before_filter :authorize, :except => :home
   def home
   	@tags = Tag.all
-
+    @post = Post.last
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @tags }
