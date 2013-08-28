fd2ac62e6dcd277bd97dbb77294d19d37c36acd9
bangarang <me@alexjrock.com>
2013-08-27 23:28:43 -0500

    moresss

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Lxrck::Application.routes.draw do
 	get '/login' => 'clearance/sessions#new', :as => 'born'
 	get '/sign_up' => 'clearance/users#new', :as => 'sign_up'
 	delete '/logout' => 'clearance/sessions#destroy', :as => 'kill'
-	resources :tags
+	resources :tags, :path => :feeds
 
 
 	# root :controller => "tags", :action => "index"
