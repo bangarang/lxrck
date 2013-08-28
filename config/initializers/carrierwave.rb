392bf95243e70d5a971a3961914f5e58c35197d3
bangarang <me@alexjrock.com>
2013-08-27 22:07:28 -0500

    carrierwave take 3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,7 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
     provider: "AWS",
     aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-    :region                 => 'us-west-2',
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
     # :host                   => 'assets.lxrck.com',
     # :endpoint               => 'http://assets.lxrck.com'
   }
